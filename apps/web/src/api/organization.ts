9db5c040319b5ddd754e4879731ed57b5c96d8de
amandhurwey <aman.dhurwey@leapfinance.com>
2022-10-01 08:57:28 +0530

    fix: revert changes

--- apps/web/src/api/organization.ts
+++ apps/web/src/api/organization.ts
@@ -1,4 +1,4 @@
-import { MemberRoleEnum } from '../../../../libs/shared';
+import { MemberRoleEnum } from '@novu/shared';
 import { api } from './api.client';
 
 export function getOrganizations() {
--- apps/web/src/api/templates.ts
+++ apps/web/src/api/templates.ts
@@ -1,4 +1,4 @@
-import { ICreateNotificationTemplateDto } from '../../../../libs/shared';
+import { ICreateNotificationTemplateDto } from '@novu/shared';
 import { api } from './api.client';
 
 export async function createTemplate(data: ICreateNotificationTemplateDto) {

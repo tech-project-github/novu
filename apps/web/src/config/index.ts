e0c508117d2eb8ed397b3eec9a4ff9c640767c98
amandhurwey <aman.dhurwey@leapfinance.com>
2022-10-04 14:07:16 +0530

    fix: use env from process.env

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -7,15 +7,21 @@ declare global {
   }
 }
 
-export const API_ROOT =
-  window._env_.REACT_APP_API_URL || (isBrowser() && (window as any).Cypress)
-    ? window._env_.REACT_APP_API_URL || 'http://localhost:1336'
-    : window._env_.REACT_APP_API_URL || 'http://localhost:3010';
+export const API_ROOT = process.env.REACT_APP_API_URL;
 
-export const WS_URL =
-  isBrowser() && (window as any).Cypress
-    ? window._env_.REACT_APP_WS_URL || process.env.REACT_APP_WS_URL || 'http://localhost:1340'
-    : window._env_.REACT_APP_WS_URL || process.env.REACT_APP_WS_URL || 'http://localhost:3002';
+/*
+ * window._env_.REACT_APP_API_URL || (isBrowser() && (window as any).Cypress)
+ *   ? window._env_.REACT_APP_API_URL || 'http://localhost:1336'
+ *   : window._env_.REACT_APP_API_URL || 'http://localhost:3010';
+ */
+
+export const WS_URL = process.env.REACT_APP_WS_URL;
+
+/*
+ * isBrowser() && (window as any).Cypress
+ *   ? window._env_.REACT_APP_WS_URL || process.env.REACT_APP_WS_URL || 'http://localhost:1340'
+ *   : window._env_.REACT_APP_WS_URL || process.env.REACT_APP_WS_URL || 'http://localhost:3002';
+ */
 
 export const SENTRY_DSN = window._env_.REACT_APP_SENTRY_DSN;
 

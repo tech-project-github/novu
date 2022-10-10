47dc79e4a4bf33b30388c3577c2775a48a01cde6
amandhurwey <aman.dhurwey@leapfinance.com>
2022-10-10 20:34:56 +0530

    fix: revert to array of urls for origin

--- apps/api/src/bootstrap.ts
+++ apps/api/src/bootstrap.ts
@@ -112,7 +112,11 @@ const corsOptionsDelegate = function (req, callback) {
   if (['dev', 'test', 'local'].includes(process.env.NODE_ENV) || isWidgetRoute(req.url)) {
     corsOptions.origin = '*';
   } else {
-    corsOptions.origin = /^(https:\/\/([^\.]*\.)?leapscholar\.com)$/i;
+    corsOptions.origin = [
+      /^(https:\/\/([^\.]*\.)?leapscholar\.com)$/i,
+      process.env.FRONT_BASE_URL,
+      process.env.WIDGET_BASE_URL,
+    ];
   }
   callback(null, corsOptions);
 };

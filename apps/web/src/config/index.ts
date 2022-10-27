2b49fb6c428710b1b7f492cea6d68afabfd3c270
amandhurwey <aman.dhurwey@leapfinance.com>
2022-10-27 10:58:54 +0530

    fix: update env and config

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -10,7 +10,7 @@ declare global {
 export const API_ROOT =
   window._env_.REACT_APP_API_URL || (isBrowser() && (window as any).Cypress)
     ? window._env_.REACT_APP_API_URL || 'http://localhost:1336'
-    : window._env_.REACT_APP_API_URL || 'http://localhost:3000';
+    : window._env_.REACT_APP_API_URL || 'http://localhost:3010';
 
 export const WS_URL =
   isBrowser() && (window as any).Cypress
